4439bbd1d8bd476673292c48c3632f5c2ab6cfe1
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-01-24 23:19:49 +0100

    Updated IPhotoGaleryHttpService service interface

--- photo_gallery/03-Shared/PhotoGalery.Http.Shared/Core/Interfaces/IPhotoGaleryHttpService.cs
+++ photo_gallery/03-Shared/PhotoGalery.Http.Shared/Core/Interfaces/IPhotoGaleryHttpService.cs
@@ -1,7 +1,15 @@
-﻿namespace PhotoGalery.Http.Shared.Core.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PhotoGallery.Shared.ApiModels.Api.PhotoAwsGallery;
+
+namespace PhotoGalery.Http.Shared.Core.Interfaces
 {
     public interface IPhotoGaleryHttpService
     {
-        
+        Task<IEnumerable<PhotoGalleryResponse>> GetAllAsync();
+
+        Task InsertAsync(InsertPhotoGralleryRequest insertPhotoGralleryRequest);
+
+        Task DeleteAsync(DeleteRequest deleteRequest);
     }
 }
