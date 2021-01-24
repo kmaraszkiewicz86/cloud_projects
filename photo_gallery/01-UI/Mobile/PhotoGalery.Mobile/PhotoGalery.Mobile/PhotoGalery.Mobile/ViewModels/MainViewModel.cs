1fe6e94c3d80f1ea8195133a3e11dddcd2a069d0
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-01-25 00:00:31 +0100

    Added basic version of main view model class

---
+++ photo_gallery/01-UI/Mobile/PhotoGalery.Mobile/PhotoGalery.Mobile/PhotoGalery.Mobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PhotoGalery.Http.Shared.Core.Implementations;
+using PhotoGallery.Shared.ApiModels.Api.PhotoAwsGallery;
+
+namespace PhotoGalery.Mobile.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<PhotoGalleryResponse> PhotoGalleryResponses { get; set; }
+
+        private readonly PhotoGaleryHttpService _photoGaleryHttpService;
+
+        public MainViewModel(PhotoGaleryHttpService photoGaleryHttpService)
+        {
+            _photoGaleryHttpService = photoGaleryHttpService;
+
+            PhotoGalleryResponses = new ObservableCollection<PhotoGalleryResponse>();
+        }
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
