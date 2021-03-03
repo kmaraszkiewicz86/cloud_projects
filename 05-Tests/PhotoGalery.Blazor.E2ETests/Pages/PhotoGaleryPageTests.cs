c1d222626313b29e7fbad3cdedc53af071d5fc85
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-03-04 00:21:19 +0100

    Removed unecessary files, Added a basic version of a blazor bb unit tests project

---
+++ 05-Tests/PhotoGalery.Blazor.E2ETests/Pages/PhotoGaleryPageTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using PhotoGalery.Http.Shared.Core.Interfaces;
+using PhotoGalery.Pages;
+using PhotoGalery.Shared.PhotoGaleryShared;
+using Xunit;
+
+namespace PhotoGalery.Blazor.E2ETests.Pages
+{
+    public class PhotoGaleryPageTests
+    {
+        [Fact]
+        public void AddNewItemTest()
+        {
+            using (var context = new TestContext())
+            {
+                var mock = new Mock<IPhotoGaleryHttpService>();
+                context.Services.AddSingleton<IPhotoGaleryHttpService>(mock.Object);
+
+                var addPhotoGaleryPage = context.RenderComponent<AddPhotoGaleryPage>();
+                //var photoGaleryPage = context.RenderComponent<PhotoGaleryPage>();
+            }
+        }
+    }
+}
